c85bc5fbf839c26834180710507b09bc8eef73d7
Etienne Belde <ejm.belde@student.avans.nl>
2017-12-12 21:09:07 +0100

    dummy data

--- model/voorstelling.model.js
+++ model/voorstelling.model.js
@@ -17,6 +17,6 @@ const voorstelling = new Voorstelling({
     name: 'Baby Boss',
     description: 'Baby die geheim agent is',
     imagePath: 'https://upload.wikimedia.org/wikipedia/en/0/0e/The_Boss_Baby_poster.jpg'
-});//.save();
+}).save();
 
 module.exports = Voorstelling;
